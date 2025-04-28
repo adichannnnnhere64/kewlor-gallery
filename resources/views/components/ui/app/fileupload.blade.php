d2136ddc3fe0a077b7ff484f38d0c4d7c29d6193
Andrew Peroramas <wolfandrew307@gmail.com>
2025-04-29 01:22:43 +0800

    fix bug on image

--- resources/views/components/ui/app/fileupload.blade.php
+++ resources/views/components/ui/app/fileupload.blade.php
@@ -21,7 +21,7 @@
     >
         <div class="space-y-2">
             <div class="flex justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" style="height:100px!important;" class=" text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
             </div>
