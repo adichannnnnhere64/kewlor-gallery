1224bb8eb02fec34262bb1223c3a09808dec7d78
Andrew Peroramas <wolfandrew307@gmail.com>
2025-05-15 06:53:45 +0800

    add horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FolioServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
     Usamamuneerchaudhary\Commentify\Providers\CommentifyServiceProvider::class,
 ];
